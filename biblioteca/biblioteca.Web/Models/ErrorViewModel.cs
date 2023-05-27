5220804dcff65afcc60b5955fe69ad186d9df83b
Angel Gabriel Valdez Ortega <20220908@itla.edu.do>
2023-05-26 23:06:23 -0400

    Primera subida.

---
+++ biblioteca/biblioteca.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace biblioteca.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
