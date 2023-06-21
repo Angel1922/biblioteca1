6cdcf561d43238b9207ef3a01fd23d6db6f44cff
elagaresm <enmanuellagares@gmail.com>
2023-06-20 20:19:48 -0400

    add infrastructure layer

---
+++ biblioteca/biblioteca.Infrastructure/Core/BaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace biblioteca.Domain.Core
+{
+    public abstract class BaseRepository<TEntity> : IRepositoryBase<TEntity> where TEntity : class
+    {
+        public virtual void Actualizar(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual List<TEntity> GetEntities()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void Guardar(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual TEntity TraerEntityPorId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ biblioteca/biblioteca.Infrastructure/Repositories/LectorRepository.cs
@@ -0,0 +1,14 @@
+﻿using biblioteca.Domain.Core;
+using biblioteca.Domain.Interfaces;
+
+namespace biblioteca.Domain.Repositories
+{
+    internal class LectorRepository : , ILectorRepository
+    {
+        public override List<Lector> GetEntities()
+        {
+            // adaptacion a nuestro requerimiento
+            return new List<LectorRepository>();
+        }
+    }
+}
